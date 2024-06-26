490f21e0c875b507ff7e56d9677d540b3f875743
daeho <daeho210@gmail.com>
2024-06-26 15:18:42 +0900

    [fix] User 모델 수정

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,22 +1,15 @@
 import pool from '../config/db';
 import { ResultSetHeader, RowDataPacket } from 'mysql2';
+import { UserData } from '../types/userData';
 
-
-interface User {
-    id?: number;
-    email: string;
-    username: string;
-    password: string;
-}
-
-const create = async (user: User): Promise<number> => {
+const create = async (user: UserData): Promise<number> => {
     const [result] = await pool.query<ResultSetHeader>('INSERT INTO Users (email, username, password) VALUES (?, ?, ?)', [user.email, user.username, user.password]);
     return result.insertId;
 };
 
-const findByEmail = async (email: string): Promise<User | undefined> => {
+const findByEmail = async (email: string): Promise<UserData | undefined> => {
     const [rows] = await pool.query<RowDataPacket[]>('SELECT * FROM Users WHERE email = ?', [email]);
-    return rows[0] as User | undefined;
+    return rows[0] as UserData | undefined;
 };
 
 const deleteAll = async () => {
--- src/types/userData.ts
+++ src/types/userData.ts
@@ -1,5 +1,6 @@
 export interface UserData {
-    id: number;
+    id?: number;
     email: string;
     username: string;
+    password?: string;
 }
